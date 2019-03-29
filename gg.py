777486d296720401897c09730f29e00a0011a54b
darcy2411 <47359710+darcy2411@users.noreply.github.com>
2019-03-29 11:42:30 -0500

    Implementacion Calcular Correspondencia
    Implementacion Calcular Correspondencia

--- gg.py
+++ gg.py
@@ -1,16 +1,20 @@
-bases = ["A","T","C","G"]
-def obtener_complemento(base):
+def calcular_correspondencia(adn1, adn2):
     """
-     (str) -> str
-
-    >>> obtener_complemento('A')
-    'T'
-    >>> obtener_complemento('T')
-    'A'
-    >>> obtener_complemento('C')
-    'G'
-    >>> obtener_complemento('G')
-    'C'
-    :param base: recibe un complemento para definir su base
-   :return: retorna el complemento de la base
-   """
+    (str,str)-> num
+    Calcula el porcentaje de correspondencia de 2 cadenas dadas
+    >>> calcular_correspondencia("AT","TA")
+    TA
+    2
+    100.0
+    >>> calcular_correspondencia("CG","GT")
+    GC
+    2
+    50.0
+    >>> calcular_correspondencia("AAA","GCG")
+    TTT
+    3
+    0.0
+    :param adn1: str:primera cadena de adn a comparar para su porcentaje
+    :param adn2: str:segunda cadena recibida para comparar su porcentaje
+    :return:num: retorna el porcentaje total en termino de reales
+    """
