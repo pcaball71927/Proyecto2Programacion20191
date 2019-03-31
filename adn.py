e32214a8d5abfcabfb42becf957882c2ec4ca4c8
darcy2411 <47359710+darcy2411@users.noreply.github.com>
2019-03-31 13:19:25 -0500

    Implementacion Cadena Valida
    Implementacion Cadena Valida

--- adn.py
+++ adn.py
@@ -63,7 +63,20 @@ def corresponden(adn1, adn2):
 
 
 def es_cadena_valida(adn):
+    """
+    (str)-> boolean
 
+    >>> cadena_valida("ACT")
+    True
+    >>> cadena_valida("XYZ")
+    False
+    >>> cadena_valida("TSG")
+    False
+
+    :param adn: String La cadena ingresada a evaluar
+    :return: retorna Boolean TRUE si la cadena es valida o FLASE si es incorrecta
+    """
+    pass
 
 
 def es_base(caracter):
--- gg.py
+++ gg.py
@@ -1,20 +1,27 @@
-def calcular_correspondencia(adn1, adn2):
+bases = ["A","T","C","G"]
+def obtener_complemento(base):
     """
-    (str,str)-> num
-    Calcula el porcentaje de correspondencia de 2 cadenas dadas
-    >>> calcular_correspondencia("AT","TA")
-    TA
-    2
-    100.0
-    >>> calcular_correspondencia("CG","GT")
-    GC
-    2
-    50.0
-    >>> calcular_correspondencia("AAA","GCG")
-    TTT
-    3
-    0.0
-    :param adn1: str:primera cadena de adn a comparar para su porcentaje
-    :param adn2: str:segunda cadena recibida para comparar su porcentaje
-    :return:num: retorna el porcentaje total en termino de reales
-    """
+     (str) -> str
+
+    >>> obtener_complemento('A')
+    'T'
+    >>> obtener_complemento('T')
+    'A'
+    >>> obtener_complemento('C')
+    'G'
+    >>> obtener_complemento('G')
+    'C'
+    :param base: recibe un complemento para definir su base
+   :return: retorna el complemento de la base
+   """
+def complemento_base(base):
+   complento = ""
+   if (base == "A"):
+     complento = complento + "T"
+   elif (base == "T"):
+     complento = complento + "A"
+   elif (base == "C"):
+     complento = complento + "G"
+   else:
+     complento = complento + "C"
+   return complento
